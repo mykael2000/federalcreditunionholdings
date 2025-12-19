6af57d43fb31dd21640229e56d88ba7e7f014ee5
Michael <eramehmichael2000@gmail.com>
2025-12-19 12:45:05 +0100

    upload

--- connection.php
+++ connection.php
@@ -6,10 +6,10 @@
 // Database connection (Change credentials accordingly)
         $servername = "localhost";
         //$username = "dualstre_Federal Credit Union Holdings";
-        $username = "dginland_federalcreditunion";
+        $username = "fede_federalholding";
         $password = "z6AwZFtHD7RPqQrM3t97";
         //$dbname = "dualstre_Federal Credit Union Holdings";
-        $dbname = "dginland_federalcreditunion";
+        $dbname = "fede_federalholding";
 
         $conn = new mysqli($servername, $username, $password, $dbname);
         // if($conn){
