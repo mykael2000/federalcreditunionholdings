3be470a9f54505e3637035d2c0134179861af6a4
Michael <eramehmichael2000@gmail.com>
2025-12-22 14:56:57 +0100

    update

--- user-area/user/dashboard/includes/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$con = mysqli_connect("lon112.truehost.cloud", "writersb_federal", "edF@8307Gh71", "writersb_federal");
-
-// if($con){
-//     echo "connection successful";
-// }else{
-//     echo "connection failed";
-// }
