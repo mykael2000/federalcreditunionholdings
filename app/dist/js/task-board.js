346aafdc5b1a038cdfc1de817b7709d89cd16fc3
Michael <eramehmichael2000@gmail.com>
2025-12-23 07:29:53 +0100

    update

--- app/dist/js/task-board.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-    "use strict";
-     $("#task-list-1, #task-list-2, #task-list-3").sortable(
-        {
-          connectWith: "#task-list-1, #task-list-2, #task-list-3",
-          items: ".task-card"
-        });
-
-})(jQuery);
